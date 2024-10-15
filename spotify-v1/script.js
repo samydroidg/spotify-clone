bfb5f5049dcd3c153b556418e20964fcd40aa80a
samydroidg <166712869+samydroidg@users.noreply.github.com>
2024-10-15 11:57:11 +0530

    Update script.js

--- spotify-v1/script.js
+++ spotify-v1/script.js
@@ -17,7 +17,7 @@ function secondsToMinutesSeconds(seconds){
 
 async function getSongs(folder) {
     currfolder = folder
-    let a = await fetch(`http://127.0.0.1:5500/${folder}/`)
+    let a = await fetch(`http://spotify-theta-fawn.vercel.app/${folder}/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
